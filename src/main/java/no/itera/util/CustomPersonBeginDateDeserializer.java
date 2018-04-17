781989e273d8c28d10d13c909c2e3ee149b0022a
Mykola Kovtun <nckkvtn@gmail.com>
2018-04-17 13:08:20 +0300

    changes

---
+++ src/main/java/no/itera/util/CustomPersonBeginDateDeserializer.java
@@ -0,0 +1,27 @@
+package no.itera.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+import static java.time.temporal.ChronoField.DAY_OF_MONTH;
+
+public class CustomPersonBeginDateDeserializer extends JsonDeserializer<LocalDate> {
+
+    public  final DateTimeFormatter dateFormatter = new DateTimeFormatterBuilder()
+            .appendPattern("MM.yyyy")
+            .parseDefaulting(DAY_OF_MONTH, 1)
+            .toFormatter();
+
+        @Override
+        public LocalDate deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            LocalDate date = LocalDate.parse(p.getValueAsString(), dateFormatter);
+            return date;
+        }
+
+}
