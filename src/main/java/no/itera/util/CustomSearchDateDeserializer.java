854aff6279929c3b0118ffb2b45e441749b86ee0
Mykola Kovtun <nckkvtn@gmail.com>
2018-04-18 11:53:04 +0300

    small fixes

--- src/main/java/no/itera/util/CustomSearchDateDeserializer.java
+++ src/main/java/no/itera/util/CustomSearchDateDeserializer.java
@@ -1,7 +1,6 @@
 package no.itera.util;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
@@ -18,23 +17,23 @@ import static java.time.temporal.ChronoField.DAY_OF_MONTH;
 
 public class CustomSearchDateDeserializer extends JsonDeserializer<SearchDate> {
 
-    DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+    private static DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
             .appendPattern("MM.yyyy")
             .parseDefaulting(DAY_OF_MONTH, 15)
             .toFormatter();
 
-    String singleDateRegex = "[0-9]{2}.[0-9]{4}";
-    String doubleDateRegex = "[0-9]{2}[.][0-9]{4}[-][0-9]{2}[.][0-9]{4}";
+    private static String SINGLE_DATE_REGEX = "[0-9]{2}.[0-9]{4}";
+    String DOUBLE_DATE_REGEX = "[0-9]{2}[.][0-9]{4}[-][0-9]{2}[.][0-9]{4}";
 
     @Override
     public SearchDate deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
         String date = jp.getValueAsString();
-        if(date.matches(singleDateRegex)){
-            return new SearchDate(LocalDate.parse(date,formatter));
+        if(date.matches(SINGLE_DATE_REGEX)){
+            return new SearchDate(LocalDate.parse(date,FORMATTER));
         }
-        else if(date.matches(doubleDateRegex)){
+        else if(date.matches(DOUBLE_DATE_REGEX)){
             List<String> dates = Arrays.asList(date.split("-"));
-            return new SearchDate(LocalDate.parse(dates.get(0),formatter),LocalDate.parse(dates.get(1),formatter));
+            return new SearchDate(LocalDate.parse(dates.get(0),FORMATTER),LocalDate.parse(dates.get(1),FORMATTER));
         }
         else{
             throw new InvalidFormatException(jp,"Error! Format is invalid",date,String.class);
