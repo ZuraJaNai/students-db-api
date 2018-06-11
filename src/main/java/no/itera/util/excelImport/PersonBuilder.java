26b3e8f382d69ea79f9abe3cebd37299d52d09f5
Mykola Kovtun <nckkvtn@gmail.com>
2018-06-11 15:30:23 +0300

    changed classes used in person builder to annonymous

--- src/main/java/no/itera/util/excelImport/PersonBuilder.java
+++ src/main/java/no/itera/util/excelImport/PersonBuilder.java
@@ -4,29 +4,70 @@ import no.itera.model.Person;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
-
 import java.time.Year;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PersonBuilder {
-    Map<String,PersonParameter> parameters;
-    Person person;
-    Row row;
-    DataFormatter dataFormatter;
+
+    private static Map<String,PersonParameter> parameters = new HashMap<String,PersonParameter>(){{
+        put(ExcelConstants.FULL_NAME,new PersonParameter(){
+            @Override
+            public Person addParameterToPersonAndReturn(String parameter, Person person) {
+                String[] fullName = parameter.split(" ");
+                person.setLastName(fullName[0]);
+                if (StringUtils.isNoneEmpty(fullName[1])) {
+                    person.setFirstName(fullName[1]);
+                } else {
+                    person.setFirstName(" ");
+                }
+                return person;
+            }
+        });
+        put(ExcelConstants.FIRST_NAME,new PersonParameter(){
+            @Override
+            public Person addParameterToPersonAndReturn(String parameter, Person person) {
+                person.setFirstName(parameter);
+                return person;
+            }
+        });
+        put(ExcelConstants.LAST_NAME,new PersonParameter(){
+            @Override
+            public Person addParameterToPersonAndReturn(String parameter, Person person) {
+                person.setLastName(parameter);
+                return person;
+            }
+        });
+        put(ExcelConstants.PATRONYMIC,new PersonParameter(){
+            @Override
+            public Person addParameterToPersonAndReturn(String parameter, Person person) {
+                person.setPatronymic(parameter);
+                return person;
+            }
+        });
+        put(ExcelConstants.EMAIL,new PersonParameter(){
+            @Override
+            public Person addParameterToPersonAndReturn(String parameter, Person person) {
+                person.setEmail(parameter);
+                return person;
+            }
+        });
+        put(ExcelConstants.YEAR, new PersonParameter() {
+            @Override
+            public Person addParameterToPersonAndReturn(String parameter, Person person) {
+                person.setYearOfStudy(parameter);
+                return person;
+            }
+        });
+    }};
+    private Person person;
+    private Row row;
+    private DataFormatter dataFormatter;
 
     public PersonBuilder(Row row){
         this.row = row;
         this.person = new Person();
         this.dataFormatter = new DataFormatter();
-        this.parameters = new HashMap<String,PersonParameter>(){{
-            put(ExcelConstants.FULL_NAME,new PersonParameterFullName());
-            put(ExcelConstants.FIRST_NAME,new PersonParameterFirstName());
-            put(ExcelConstants.LAST_NAME,new PersonParameterLastName());
-            put(ExcelConstants.PATRONYMIC,new PersonParameterPatronymic());
-            put(ExcelConstants.EMAIL,new PersonParameterEmail());
-            put(ExcelConstants.YEAR,new PersonParameterYear());
-        }};
     }
 
     public Person getPerson() {
