c0981e6257f1ac7bc4fdebc09fcf16e1e2d28819
Mykola Kovtun <nckkvtn@gmail.com>
2018-12-18 12:18:07 +0200

    git changes

--- docker-build.bat
+++
@@ -1,8 +0,0 @@
-rem call mvn clean install
-docker build -f Dockerfile -t students-db-api .
-docker save students-db-api > "target\students-db-api.tar"
-echo off
-echo *************************************************
-echo                   JOB IS DONE
-echo *************************************************
-pause
